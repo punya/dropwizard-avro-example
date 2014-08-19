6933076f2d3cef7ec29f72669df5ea386a915ba5
Punya Biswal <pbiswal@palantir.com>
2014-08-19 07:56:57 -0700

    Improve FE code

--- translate-webserver/src/main/resources/assets/app.js
+++ translate-webserver/src/main/resources/assets/app.js
@@ -1,11 +1,15 @@
-var app = angular.module('translate', []);
+(function() {
+  var app = angular.module('translate', []);
 
-function TranslateController($http, $scope) {
-  $scope.$watch('source', function(source) {
-    $http.post('api/translate', source).success(function(outputs) {
-      $scope.outputs = outputs;
+  app.controller('TranslateController', ['$http', '$rootScope', function($http, $rootScope) {
+    var self = this;
+    this.outputs = [];
+    $rootScope.$watch(function() {
+      return self.source;
+    }, function(source) {
+      $http.post('api/translate', source).success(function(outputs) {
+        self.outputs = outputs;
+      });
     });
-  });
-}
-
-app.controller('TranslateController', TranslateController);
+  }]);
+})();
